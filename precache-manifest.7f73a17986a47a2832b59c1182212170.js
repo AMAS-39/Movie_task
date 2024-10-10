3224a58f99c05f9d0424124fed6fa6490e920c21
Ahmad Shwan <Ahmadshwanaswad@gmail.com>
2024-10-11 01:37:39 +0300

    Updates

---
+++ precache-manifest.7f73a17986a47a2832b59c1182212170.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de062c881592c375013ba0693442d093",
+    "url": "/Movie_task/index.html"
+  },
+  {
+    "revision": "a6c97f3537e856084f63",
+    "url": "/Movie_task/static/css/main.19e63a85.chunk.css"
+  },
+  {
+    "revision": "10f5c1c48b0df52e5a20",
+    "url": "/Movie_task/static/js/2.57155e08.chunk.js"
+  },
+  {
+    "revision": "afae8725e7d5172c6ee86f29e0116382",
+    "url": "/Movie_task/static/js/2.57155e08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6c97f3537e856084f63",
+    "url": "/Movie_task/static/js/main.87ce6fb1.chunk.js"
+  },
+  {
+    "revision": "0eca505434e08ee2fd02",
+    "url": "/Movie_task/static/js/runtime-main.6125b370.js"
+  }
+]);
--- src/index.js
+++
@@ -1,9 +0,0 @@
-// src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import './theme.css'; // Add this line to import theme.css
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
